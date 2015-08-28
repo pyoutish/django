24fee2aa8144759f4a2d25b55e34547a6d46294b
pyoutish <pyoutish@intersog.com>
2015-08-28 19:52:21 +0300

    #

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite',
+        'USER': 'django',
+        'PASSWORD':'password',
+        'DATABASE_HOST':'/tmp/mysql.sock',
+        'PORT':'3306',
     }
 }
 
